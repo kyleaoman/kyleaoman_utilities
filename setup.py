d84964a0460ede912506534544d71f8758e34c2f
K.A. Oman <koman@astro.rug.nl>
2017-10-24 09:59:27 +0200

    Renamed module to kyleaoman_utilities.

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     author='Kyle Oman',
     author_email='koman@astro.rug.nl',
     license='',
-    packages=['utilities'],
-    install_requires=['numpy', 'astropy', 'matplotlib'],
+    packages=['kyleaoman_utilities'],
+    install_requires=['numpy', 'astropy', 'matplotlib', 'h5py'],
     include_package_data=True,
     zip_safe=False
 )
