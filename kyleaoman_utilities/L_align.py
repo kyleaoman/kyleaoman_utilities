a2a68779ac7170caa2f6522980ccb6784435b71f
K.A. Oman <koman@astro.rug.nl>
2017-10-31 16:21:51 +0100

    Initial working version of L_align.

--- kyleaoman_utilities/L_align.py
+++ kyleaoman_utilities/L_align.py
@@ -1,22 +1,24 @@
 import numpy as np
 
-def L_align(xyz, vxyz, m, frac=.3, saverot=None, Laxis=(0, 0, 1), xaxis=None):
+def L_align(xyz, vxyz, m, frac=.3, saverot=None, Laxis='z'):
 
     transposed = False
     if xyz.ndim != 2:
         raise ValueError('L_align: cannot guess coordinate axis for input with ndim != 2.')
-    else if (xyz.shape[0] == 3) and (xyz.shape[1] == 3):
+    elif (xyz.shape[0] == 3) and (xyz.shape[1] == 3):
         raise ValueError('L_align: cannot guess coordinate axis for input with shape (3, 3).')
-    else if xyz.shape[1] == 3:
+    elif xyz.shape[1] == 3:
         xyz = xyz.T
         vxyz = vxyz.T
         transposed = True
+    elif (xyz.shape[0] != 3) and (xyz.shape[1] != 3):
+        raise ValueError('L_align: coordinate array shape '+str(xyz.shape)+' invalid (one dim must be 3).')
 
     rsort = np.argsort(np.sum(np.power(xyz, 2), axis=0), kind='quicksort')
-    p = m[:, np.newaxis] * vxyz
-    L = np.cross(xyz, p)
-    p = p[rsort]
-    L = L[rsort]
+    p = m[np.newaxis] * vxyz
+    L = np.cross(xyz, p, axis=0)
+    p = p[:, rsort]
+    L = L[:, rsort]
     m = m[rsort]
     mcumul = np.cumsum(m) / np.sum(m)
     Nfrac = np.argmin(np.abs(mcumul - frac))
@@ -26,12 +28,25 @@ def L_align(xyz, vxyz, m, frac=.3, saverot=None, Laxis=(0, 0, 1), xaxis=None):
     L = L[:Nfrac]
     Ltot = np.sqrt(np.sum(np.power(np.sum(L, axis=1), 2)))
     Lhat = np.sum(L, axis=1) / Ltot
-    zhat = Laxis / np.sqrt(np.sum(np.power(Laxis, 2))) #normalized
-    xaxis = np.array([1., 1., 1.]) if xaxis is None else np.array(xaxis) #default unlikely Laxis
+    zhat = Lhat / np.sqrt(np.sum(np.power(Lhat, 2))) #normalized
+    xaxis = np.array([1., 1., 1.]) #default unlikely Laxis
     xhat = xaxis - xaxis.dot(zhat) * zhat
     xhat = xhat / np.sqrt(np.sum(np.power(xhat, 2))) #normalized
     yhat = np.cross(zhat, xhat) #guarantees right-handedness
 
     rotmat = np.vstack((xhat, yhat, zhat)) #units will be dropped (which is good)
-    np.save(rotmat, saverot)
+    if Laxis == 'z':
+        pass
+    elif Laxis == 'y':
+        rotmat = np.roll(rotmat, 2, axis=0)
+    elif Laxis == 'x':
+        rotmat = np.roll(rotmat, 1, axis=0)
+    else:
+        raise ValueError("L_align: Laxis must be one of 'x', 'y' or 'z'.")
+
+    if transposed == True:
+        rotmat = rotmat.T
+    if saverot is not None:
+        np.save(saverot, rotmat)
+
     return rotmat
