656af4efea0e1b99cbbd14cce0e45751340c300d
Kyle Oman <koman@astro.rug.nl>
2018-01-17 05:31:34 -0800

    Always intervals, with default read all.

--- kyleaoman_utilities/hdf5_io.py
+++ kyleaoman_utilities/hdf5_io.py
@@ -2,27 +2,15 @@ import h5py as h5py
 import numpy as np
 import multiprocessing
 import os.path
-import warnings
 
 class _hdf5_io():
 
-    def __init__(self, path, fbase, ncpu=0, interval=False):
+    def __init__(self, path, fbase, ncpu=0, interval=None):
         self._path = path
         self._fbase = fbase
         self._parts = self._find_parts(self._path, self._fbase)
         self._nb_cpu = multiprocessing.cpu_count() - 1 if ncpu == 0 else ncpu
         self._interval = interval
-
-    def _subitem(self, name, parts, output):
-        accumulator = []
-        for part in parts:
-            with h5py.File(part, 'r') as f:
-                try:
-                    accumulator.append(f[name].value.copy())
-                except KeyError:
-                    pass
-        output.put(accumulator)
-        return
 
     def _subitem_interval(self, name, parts, output, intervals):
         accumulator = []
@@ -34,30 +22,24 @@ class _hdf5_io():
             
 
     def __getitem__(self, name):
-        if self._interval != False:
-            interval_parts = self._split_interval(name)
+        items = []
+        all_interval_parts = self._split_interval(name)
+        all_parts = [p for p, i in zip(self._parts, all_interval_parts) if i != False]
+        all_interval_parts = [i for i in all_interval_parts if i != False]
         if self._nb_cpu > 1:
+            parts_split = np.array_split(all_parts, self._nb_cpu)
+            interval_parts_split = np.array_split(all_interval_parts, self._nb_cpu)
+            procs = []
+            outputs = []
             try:
-                if self._interval == False:
-                    parts_split = np.array_split(self._parts, self._nb_cpu)
-                else:
-                    _parts = [p for p, i in zip(self._parts, interval_parts) if i != False]
-                    interval_parts = [i for i in interval_parts if i != False]
-                    parts_split = np.array_split(_parts, self._nb_cpu)
-                    interval_parts_split = np.array_split(interval_parts, self._nb_cpu)
-                procs = []
-                outputs = []
-                for pn, parts in enumerate(parts_split):
+                for parts, interval_parts in zip(parts_split, interval_parts_split):
                     outputs.append(multiprocessing.Queue())
-                    target = self._subitem if self._interval == False else self._subitem_interval
-                    args = (name, parts.tolist(), outputs[-1])
-                    if self._interval != False:
-                        args = args + (interval_parts_split[pn].tolist(), )
+                    target = self._subitem_interval
+                    args = (name, parts.tolist(), outputs[-1], interval_parts.tolist())
                     procs.append(
                         multiprocessing.Process(target=target, args=args)
                     )
                     procs[-1].start()
-                items = []
                 for output in outputs:
                     items += output.get()
                 for p in procs:
@@ -66,20 +48,10 @@ class _hdf5_io():
                 self._nb_cpu = 1 #fallback to serial mode
                 return self[name]
         else:
-            items = []
-            for pn, part in enumerate(self._parts):
+            for part, interval_part in zip(all_parts, all_interval_parts):
                 with h5py.File(part, 'r') as f:
-                    if self._interval == False:
-                        try:
-                            items.append(f[name].value.copy())
-                        except KeyError:
-                            pass
-                    else:
-                        if interval_parts[pn] == False:
-                            continue
-                        else:
-                            startslice, endslice = interval_parts[pn]
-                            items.append(f[name][startslice : endslice].copy())
+                    startslice, endslice = interval_part
+                    items.append(f[name][startslice : endslice].copy())
         if len(items) == 0:
             raise KeyError("Unable to open object (Object '" + name + \
                            "' doesn't exist in file with path '" + self._path + \
@@ -97,22 +69,26 @@ class _hdf5_io():
                 except KeyError:
                     slices.append(False)
                     continue
-                if self._interval[0] <= start:
+                if self._interval != None:
+                    if self._interval[0] <= start:
+                        startslice = 0
+                    elif self._interval[0] <= end:
+                        startslice = self._interval[0] - start
+                    else:
+                        slices.append(False)
+                        start = end
+                        continue
+                    if self._interval[1] >= end:
+                        endslice = end - start
+                    elif self._interval[1] >= start:
+                        endslice = self._interval[1] - start
+                    else:
+                        slices.append(False)
+                        start = end
+                        continue
+                else:
                     startslice = 0
-                elif self._interval[0] <= end:
-                    startslice = self._interval[0] - start
-                else:
-                    slices.append(False)
-                    start = end
-                    continue
-                if self._interval[1] >= end:
-                    endslice = end
-                elif self._interval[1] >= start:
-                    endslice = self._interval[1] - start
-                else:
-                    slices.append(False)
-                    start = end
-                    continue
+                    endslice = end - start
                 slices.append((startslice, endslice))
                 start = end
         return slices
@@ -134,7 +110,7 @@ class _hdf5_io():
     def get_parts(self):
         return self._parts
 
-def hdf5_get(path, fbase, hpath, attr=None, ncpu=0, interval=False):
+def hdf5_get(path, fbase, hpath, attr=None, ncpu=0, interval=None):
     '''
     path: directory containing hdf5 file
     fbase: filename (omit '.X.hdf5' portion)
@@ -155,3 +131,31 @@ def hdf5_get(path, fbase, hpath, attr=None, ncpu=0, interval=False):
         raise KeyError("Unable to open attribute (One of object '" + hpath + \
                        "' or attribute '" + attr + "' doesn't exist in file with path '" + path + \
                        "' and basename '" + fbase + "')")
+
+"""
+#TESTS ON CAVI
+
+import time
+
+path = '/sraid14/azadehf/LG/data_fix/V1_LR_fix/particledata_127_z000p000'
+fbase = 'eagle_subfind_particles_127_z000p000'
+hpath = '/PartType1/Coordinates'
+
+interval = (20000, 100000)
+
+t0 = time.clock()
+d1 = hdf5_get(path, fbase, hpath, ncpu=1)[interval[0] : interval[1]]
+t1 = time.clock()
+d2 = hdf5_get(path, fbase, hpath, ncpu=2)[interval[0] : interval[1]]
+t2 = time.clock()
+d3 = hdf5_get(path, fbase, hpath, ncpu=1, interval=interval)
+t3 = time.clock()
+d4 = hdf5_get(path, fbase, hpath, ncpu=2, interval=interval)
+t4 = time.clock()
+
+print(interval[1] - interval[0] == d1.shape[0])
+print((d1 == d2).all(), (d1 == d3).all(), (d1 == d4).all())
+print(t1-t0 > t2-t1, t2 - t1 > t3-t2, t3-t2 > t4-t3)
+
+
+"""
