82e39232a8f0739f324ea4c1dcfb5f676c8f9ea9
Kyle Oman <koman@astro.rug.nl>
2018-01-17 03:44:42 -0800

    Efficient reading intervals (serial).

--- kyleaoman_utilities/hdf5_io.py
+++ kyleaoman_utilities/hdf5_io.py
@@ -2,14 +2,17 @@ import h5py as h5py
 import numpy as np
 import multiprocessing
 import os.path
+import warnings
 
 class _hdf5_io():
 
-    def __init__(self, path, fbase, ncpu=0):
+    def __init__(self, path, fbase, ncpu=0, interval=False):
         self.__path = path
         self.__fbase = fbase
         self.__parts = self.__find_parts(self.__path, self.__fbase)
         self.__nb_cpu = multiprocessing.cpu_count() - 1 if ncpu == 0 else ncpu
+        self.__interval = interval
+        self.__nb_cpu = 1 if self.__interval != False else self.__nb_cpu
 
     def __subitem(self, name, parts, output):
         accumulator = []
@@ -47,12 +50,34 @@ class _hdf5_io():
                 return self[name]
         else:
             items = []
+            start = 0
             for part in self.__parts:
                 with h5py.File(part, 'r') as f:
-                    try:
-                        items.append(f[name].value.copy())
-                    except KeyError:
-                        pass
+                    if self.__interval == False:
+                        try:
+                            items.append(f[name].value.copy())
+                        except KeyError:
+                            pass
+                    else:
+                        end = start + f[name].shape[0]
+                        if self.__interval[0] <= start:
+                            startslice = 0
+                        elif self.__interval[0] <= end:
+                            startslice = self.__interval[0] - start
+                        else:
+                            items.append(np.empty((0, ) + f[name].shape[1:]))
+                            start = end
+                            continue
+                        if self.__interval[1] >= end:
+                            endslice = end
+                        elif self.__interval[1] >= start:
+                            endslice = self.__interval[1] - start
+                        else:
+                            items.append(np.empty((0, ) + f[name].shape[1:]))
+                            start = end
+                            continue
+                        items.append(f[name][startslice : endslice].copy())
+                        start = end
         if len(items) == 0:
             raise KeyError("Unable to open object (Object '" + name + \
                            "' doesn't exist in file with path '" + self.__path + \
@@ -77,7 +102,7 @@ class _hdf5_io():
     def get_parts(self):
         return self.__parts
 
-def hdf5_get(path, fbase, hpath, attr=None, ncpu=0):
+def hdf5_get(path, fbase, hpath, attr=None, ncpu=0, interval=False):
     '''
     path: directory containing hdf5 file
     fbase: filename (omit '.X.hdf5' portion)
@@ -85,7 +110,10 @@ def hdf5_get(path, fbase, hpath, attr=None, ncpu=0):
     attr: name of attribute to fetch (optional)
     '''
     if not attr:
-        hdf5_file = _hdf5_io(path, fbase, ncpu=ncpu)
+        if (interval != False) and (ncpu != 1):
+            warnings.warn("Using interval with hdf5_get must use ncpu=1, proceeding with serial execution.")
+            ncpu = 1
+        hdf5_file = _hdf5_io(path, fbase, ncpu=ncpu, interval=interval)
         retval = hdf5_file[hpath]
         return retval
     else:
