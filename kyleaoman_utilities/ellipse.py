6dea4e4b8c195867d5beecc174a7d35466fe4246
K.A. Oman <koman@astro.rug.nl>
2018-01-25 10:05:29 +0100

    Corrections to reference angle in ellipse.py.

--- kyleaoman_utilities/ellipse.py
+++ kyleaoman_utilities/ellipse.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def getR(x, y, incl=0., PA=np.radians(270.), x0=0., y0=0.):
-    PA -= 3. * np.pi / 2.
+    #PA -= 3. * np.pi / 2.
     return np.sqrt(
         np.power((x - x0) * np.cos(PA) + (y - y0) * np.sin(PA), 2) +
         np.power((x - x0) * np.sin(PA) - (y - y0) * np.cos(PA), 2)
@@ -14,7 +14,7 @@ def phi_to_t(phi, incl=None, PA=0.):
     #PROVIDE PA ONLY FOR PRODUCING A t_grid, e.g. phi_to_t(phi_grid) where phi_grid = np.arctan2(*np.meshgrid(xs, ys)[::-1])
     if incl is None:
         raise ValueError
-    PA = PA - 3 * np.pi / 2.
+    #PA = PA - 3. * np.pi / 2.
     phi = phi - PA
     retval = np.arctan(np.tan(phi) / np.cos(incl)) + np.pi * np.trunc(np.sign(phi) * (np.abs(phi) + np.pi / 2.) / np.pi)
     while (retval < 0.).any():
@@ -31,7 +31,7 @@ def getx(t=None, phi=None, incl=0., PA=np.radians(270.), x0=0., y0=0., R=1.):
     else:
         raise ValueError
         
-    PA -= 3. * np.pi / 2.
+    #PA -= 3. * np.pi / 2.
     return R * np.cos(t) * np.cos(PA) - R * np.cos(incl) * np.sin(t) * np.sin(PA) + x0
 
 def gety(t=None, phi=None, incl=0., PA=np.radians(270.), x0=0., y0=0., R=1.):
@@ -42,7 +42,7 @@ def gety(t=None, phi=None, incl=0., PA=np.radians(270.), x0=0., y0=0., R=1.):
     else:
         raise ValueError
     
-    PA -= 3 * np.pi / 2.
+    #PA -= 3 * np.pi / 2.
     return R * np.cos(t) * np.sin(PA) + R * np.cos(incl) * np.sin(t) * np.cos(PA) + y0
 
 def getR_fromphi(t=None, phi=None, incl=0., PA=np.radians(270.), x0=0., y0=0., R=1.):
