3cd73b003ac94cee4b293cc8a5df58327280fce9
Kyle Oman <koman@astro.rug.nl>
2018-10-11 12:27:42 +0100

    Added max memory usage utility.

--- kyleaoman_utilities/memusg.py
+++ kyleaoman_utilities/memusg.py
@@ -1,5 +1,6 @@
 import os
 import psutil
+import resource
 
 prefixes = ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']
 
@@ -12,3 +13,12 @@ def memnow():
         rss /= 1024
         ui += 1
     return '{:.1f} {:s}B'.format(rss, prefixes[ui])
+
+
+def memmax():
+    maxrss = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+    ui = 1
+    while maxrss > 1024:
+        maxrss /= 1024
+        ui += 1
+    return '{:.1f} {:s}B'.format(maxrss, prefixes[ui])
