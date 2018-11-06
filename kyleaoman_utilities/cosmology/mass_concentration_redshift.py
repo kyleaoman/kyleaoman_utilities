a503abe79bd37581cff3b882e161098fb9737735
K.A. Oman <koman@astro.rug.nl>
2018-11-06 12:55:22 +0100

    Various bugfixes, cleanup.

--- kyleaoman_utilities/cosmology/mass_concentration_redshift.py
+++ kyleaoman_utilities/cosmology/mass_concentration_redshift.py
@@ -6,45 +6,64 @@ from .params import WMAP7
 
 prefix = os.path.dirname(__file__) + '/data/'
 
+
 def _load_tables(CP=WMAP7()):
     try:
-        sigma0_tab,M_tab_s = np.genfromtxt(prefix + 'sigma0_' + CP.name + '.dat').T
+        sigma0_tab, M_tab_s = \
+            np.genfromtxt(prefix + 'sigma0_' + CP.name + '.dat').T
         M_tab_c, c_tab = np.genfromtxt(prefix + 'Mc_z0_' + CP.name + '.dat').T
     except IOError:
-        raise IOError('Missing tables, maybe they are unavailable for this cosmology (supported: WMAP7, Planck13).')
+        raise IOError('Missing tables, maybe they are unavailable for this'
+                      ' cosmology (supported: WMAP7, Planck13).')
     M_tab_s = np.power(10, M_tab_s) * 1E10 / CP.h
     M_tab_c = M_tab_c * 1E10 / CP.h
     return sigma0_tab, M_tab_s, M_tab_c, c_tab
-            
+
 
 def _D(z, CP=WMAP7()):
     Ez = np.sqrt(CP.Ol + CP.Om * np.power(1 + z, 3))
     OmegaL_z = CP.Ol / np.power(Ez, 2)
     OmegaM_z = 1 - OmegaL_z
-    g0 = 2.5 * CP.Om / (np.power(CP.Om, 4. / 7.) - CP.Ol + (1. + .5 * CP.Om) * (1 + CP.Ol / 70.))
-    gz = 2.5 * OmegaM_z / (np.power(OmegaM_z, 4. / 7.) - OmegaL_z + (1. + .5 * OmegaM_z) * (1 + OmegaL_z / 70.))
+    g0 = 2.5 * CP.Om / (
+        np.power(CP.Om, 4. / 7.) -
+        CP.Ol +
+        (1. + .5 * CP.Om) * (1 + CP.Ol / 70.)
+    )
+    gz = 2.5 * OmegaM_z / (
+        np.power(OmegaM_z, 4. / 7.) -
+        OmegaL_z +
+        (1. + .5 * OmegaM_z) * (1 + OmegaL_z / 70.)
+    )
     return gz / (g0 * (1 + z))
 
-def _zf(M0, CP=WMAP7()):
-    f = 0.068
-    f_zf = lambda x: _D(x, CP=CP) - np.power(np.power(_D(0, CP=CP), -1) + (.477 / (.15 * np.power(12 * np.pi, 2. / 3.) * np.power(CP.Om, 0.0055))) * np.sqrt(2 * (np.power(sigma(f * M0), 2) - np.power(sigma(M0), 2))), -1)
-    return fsolve(f_zf, 2.)
-
-'''
-def chi(M0, CP=WMAP7()):
-    return 1.211 + 1.858 * np.log10(1 + _zf(M0, CP=CP)) + 0.308 * np.power(CP.Ol, 2) - 0.032 * np.log10(M0 * CP.h / 1E11)
-'''
 
 def _sigma(M0, CP=WMAP7()):
     sigma0_tab, M_tab_s, M_tab_c, c_tab = _load_tables(CP=CP)
     f_sigma = interpolate(M_tab_s, sigma0_tab)
     return f_sigma(M0)
 
+
+def _zf(M0, CP=WMAP7()):
+    f = 0.068
+
+    def f_zf(x):
+        _D(x, CP=CP) - np.power(
+            np.power(_D(0, CP=CP), -1) +
+            (.477 / (.15 * np.power(12 * np.pi, 2. / 3.) *
+                     np.power(CP.Om, 0.0055)))
+            * np.sqrt(2 * (np.power(_sigma(f * M0), 2) -
+                           np.power(_sigma(M0), 2))),
+            -1
+        )
+    return fsolve(f_zf, 2.)
+
+
 def c(Mz, z=0, CP=WMAP7()):
     if z == 0:
         M0 = Mz
     else:
-        f_M0 = lambda x: _sigma(x, CP=CP) - _sigma(Mz, CP=CP) * _D(z, CP=CP) / _D(0, CP=CP)
+        def f_M0(x):
+            _sigma(x, CP=CP) - _sigma(Mz, CP=CP) * _D(z, CP=CP) / _D(0, CP=CP)
         M0 = fsolve(f_M0, Mz)
     sigma0_tab, M_tab_s, M_tab_c, c_tab = _load_tables(CP=CP)
     f_c = interpolate(M_tab_c, c_tab)
--- kyleaoman_utilities/cosmology/rvir_conventions.py
+++ kyleaoman_utilities/cosmology/rvir_conventions.py
@@ -1,26 +1,36 @@
 import numpy as np
 from scipy.optimize import fsolve
 from .params import WMAP7
-from .mass_concentration_redshift import c as concentration
+
 
 def rvir_ratio(DeltaA, rel_toA, DeltaB, rel_toB, CP=WMAP7(), c=None):
     if c is None:
         raise ValueError("Provide concentration parameter (kwarg 'c').")
-    overdensity_ratio = DeltaA * {'b': CP.Om, 'c': 1}[rel_toA] / (DeltaB * {'b': CP.Om, 'c': 1}[rel_toB])
-    f = lambda r_ratio: np.power(r_ratio, 3) - np.power(overdensity_ratio, -1) * (np.log(1 + c) - c / (1 + c)) / (np.log(1 + c / r_ratio) - c / (r_ratio + c))
+    overdensity_ratio = DeltaA * {'b': CP.Om, 'c': 1}[rel_toA] / \
+        (DeltaB * {'b': CP.Om, 'c': 1}[rel_toB])
+
+    def f(r_ratio):
+        return np.power(r_ratio, 3) - np.power(overdensity_ratio, -1) * \
+            (np.log(1 + c) - c / (1 + c)) / (np.log(1 + c / r_ratio) -
+                                             c / (r_ratio + c))
+
     try:
-        guess = np.ones((c * DeltaA * DeltaB).shape) #these should be broadcastable, or scalar
+        # these should be broadcastable, or scalar
+        guess = np.ones((c * DeltaA * DeltaB).shape)
     except AttributeError:
         guess = 1.0
     return fsolve(f, x0=guess)
 
+
 def mvir_ratio(DeltaA, rel_toA, DeltaB, rel_toB, CP=WMAP7(), c=None):
-    overdensity_ratio = DeltaA * {'b': CP.Om, 'c': 1}[rel_toA] / (DeltaB * {'b': CP.Om, 'c': 1}[rel_toB])
+    overdensity_ratio = DeltaA * {'b': CP.Om, 'c': 1}[rel_toA] / \
+        (DeltaB * {'b': CP.Om, 'c': 1}[rel_toB])
     r_ratio = rvir_ratio(DeltaA, rel_toA, DeltaB, rel_toB, CP=CP, c=c)
     return overdensity_ratio * np.power(r_ratio, 3)
 
+
 def Delta_vir(z, CP=WMAP7()):
-    #Bryan & Norman (1998)
+    # Bryan & Norman (1998)
     x = -np.power(1 + CP.Om / CP.Ol * np.power(1 + z, 3), -1)
     delta_c = 18. * np.power(np.pi, 2) + 82. * x - 39. * np.power(x, 2)
-    return delta_c / CP.Om #delta_b
+    return delta_c / CP.Om  # delta_b
